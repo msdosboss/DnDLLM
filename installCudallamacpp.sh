cff0c1c9924cce102b0744826a1fc76be4ee8339
msdosboss <jacobt0722@gmail.com>
2025-04-24 10:37:38 -0700

    updated pip freeze and created sh script to install cuda version of llama.cpp from pip

--- installCudallamacpp.sh
+++ installCudallamacpp.sh
@@ -1,1 +1,2 @@
+pip uninstall llama-cpp-python -y
 CMAKE_ARGS="-DGGML_CUDA=on -DCUDA_PATH=/usr/local/cuda-12.2 -DCUDAToolkit_ROOT=/usr/local/cuda-12.2 -DCUDAToolkit_INCLUDE_DIR=/usr/local/cuda-12/include -DCUDAToolkit_LIBRARY_DIR=/usr/local/cuda-12.2/lib64" FORCE_CMAKE=1 pip install llama-cpp-python --no-cache-dir
